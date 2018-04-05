bfc8024fd14ceed486462fd9dba82f50a80d9f6f
Arief Adjie Wicaksono <id.ariefadjie@gmail.com>
2018-04-05 14:25:07 +0700

    update seeder

--- database/seeds/PlacesTableSeeder.php
+++ database/seeds/PlacesTableSeeder.php
@@ -19,7 +19,7 @@ class PlacesTableSeeder extends Seeder
                 'name'=>'Titik Singgah',
                 'location' => 'Jl. Ki Hajar Dewantara',
                 'description' => 'Ut eiusmod eiusmod magna proident commodo enim dolore occaecat consectetur eu quis exercitation pariatur.',
-                'img_profile' => 'https://lorempixel.com/400/200/food/1',
+                'img_profile' => 'https://lorempixel.com/100/100/food/1',
                 'img_cover' => 'https://lorempixel.com/400/200/food/1',
             ],
             [
@@ -27,9 +27,25 @@ class PlacesTableSeeder extends Seeder
                 'name'=>'Kayu Watu',
                 'location' => 'Jl. Ki Hajar Dewantara Gang Teladan',
                 'description' => 'Nisi velit Lorem culpa nulla mollit aute dolor commodo eu eiusmod enim in.',
-                'img_profile' => 'https://lorempixel.com/400/200/food/2',
+                'img_profile' => 'https://lorempixel.com/100/100/food/2',
                 'img_cover' => 'https://lorempixel.com/400/200/food/2',
-            ]
+            ],
+            [
+                'id' => 3,
+                'name'=>'Griya Kebun',
+                'location' => 'Jl. Ki Hajar Dewantara',
+                'description' => 'Duis dolore ipsum anim ad exercitation anim..',
+                'img_profile' => 'https://lorempixel.com/100/100/nature/3',
+                'img_cover' => 'https://lorempixel.com/400/200/nature/3',
+            ],
+            [
+                'id' => 4,
+                'name'=>'Istana Resto',
+                'location' => 'Jl. A. Yani',
+                'description' => 'Aute nostrud esse duis laboris ipsum incididunt occaecat in proident consectetur.',
+                'img_profile' => 'https://lorempixel.com/100/100/food/3',
+                'img_cover' => 'https://lorempixel.com/400/200/food/4',
+            ],
         ]);
     }
 }
